7b13e2472c0b83d59a4c92aff213d0522fc81063
Ole <ole.juergensen87@gmail.com>
2020-11-19 14:01:17 +0100

    Add GameObject destruction utility

---
+++ Assets/MufflonUtil/Runtime/DestructionUtil.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace MufflonUtil
+{
+    [CreateAssetMenu(menuName = "MufflonUtil/Destruction Util", fileName = "DestructionUtil")]
+    public class DestructionUtil : ScriptableObject
+    {
+        [UsedImplicitly]
+        public void Destroy(GameObject gameObject)
+        {
+            if (gameObject != null) Object.Destroy(gameObject);
+        }
+    }
+}

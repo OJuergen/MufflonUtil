7b3a3e4cb3476f104e3519d45cda41770abece56
Ole <ole.juergensen87@gmail.com>
2020-12-15 16:26:32 +0100

    Improvements to go destruction util code

--- Assets/MufflonUtil/Runtime/DestructionUtil.cs
+++ Assets/MufflonUtil/Runtime/DestructionUtil.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 namespace MufflonUtil
 {
+    /// <summary>
+    /// Utility asset to allow destroying objects with a non-static method that can be used from Unity Events.
+    /// </summary>
     [CreateAssetMenu(menuName = "MufflonUtil/Destruction Util", fileName = "DestructionUtil")]
     public class DestructionUtil : ScriptableObject
     {
         [UsedImplicitly]
-        public void Destroy(GameObject gameObject)
+        public new void Destroy(Object objectToDestroy)
         {
-            if (gameObject != null) Object.Destroy(gameObject);
+            if (objectToDestroy != null) Object.Destroy(objectToDestroy);
         }
     }
 }

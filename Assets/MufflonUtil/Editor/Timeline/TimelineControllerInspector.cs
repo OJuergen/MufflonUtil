50ed47f34043dc79f8cf2b03207c8c5f4f8c22eb
Ole <ole.juergensen87@gmail.com>
2021-07-23 15:25:46 +0200

    Timeline Controller Inspector

---
+++ Assets/MufflonUtil/Editor/Timeline/TimelineControllerInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MufflonUtil
+{
+    [CustomEditor(typeof(TimelineController))]
+    public class TimelineControllerInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if(GUILayout.Button("Break Loop")) ((TimelineController) target).BreakLoop();
+            if(GUILayout.Button("Jump")) ((TimelineController) target).Jump();
+        }
+    }
+}

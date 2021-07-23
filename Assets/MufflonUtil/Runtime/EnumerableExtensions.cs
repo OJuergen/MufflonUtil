4d69e43c3bf60ad1a2ea37f5d8a7aeddd6cd7632
Ole <ole.juergensen87@gmail.com>
2021-07-23 15:15:00 +0200

    More Enumerable extensions

--- Assets/MufflonUtil/Runtime/EnumerableExtensions.cs
+++ Assets/MufflonUtil/Runtime/EnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace MufflonUtil
@@ -12,6 +13,23 @@ namespace MufflonUtil
         public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer)
         {
             return new HashSet<T>(source, comparer);
+        }
+        
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (T element in enumerable)
+            {
+                action(element);
+            }
+        }
+        
+        public static IEnumerable<T> Apply<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (T element in enumerable)
+            {
+                action(element);
+                yield return element;
+            }
         }
     }
 }

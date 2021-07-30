fe6c40272ae0b1b7b304234b31a641ad3ec418d9
Ole <ole.juergensen87@gmail.com>
2021-07-30 10:49:08 +0200

    Improved FollowCameraPlayable

--- Assets/MufflonUtil/Runtime/Timeline/FollowCameraPlayable.cs
+++ Assets/MufflonUtil/Runtime/Timeline/FollowCameraPlayable.cs
@@ -21,26 +21,29 @@ namespace MufflonUtil
         [SerializeField] private float _lerpFactor = 5;
         [SerializeField] private Vector3 _relativeSetpointPosition = new Vector3(0, -0.1f, 0.5f);
         [SerializeField] private bool _executeInEditMode;
+        [SerializeField] private bool _faceAwayFromCamera;
         private Transform _mainCamTransform;
 
         protected override void OnBehaviourStart(Transform transform)
         {
-            var mainCam = Camera.main;
+            Camera mainCam = Camera.main;
             _mainCamTransform = mainCam != null ? mainCam.transform : null;
         }
 
         protected override void OnBehaviourUpdate(Playable playable, FrameData info, Transform transform)
         {
-            if (!Application.isPlaying && !_executeInEditMode) return;
-            var cameraPosition = _mainCamTransform.position;
+            if (!Application.isPlaying && !_executeInEditMode || _mainCamTransform == null) return;
+            Vector3 cameraPosition = _mainCamTransform.position;
 
-            var currentPosition = transform.position;
-            var setpointPosition = cameraPosition + _mainCamTransform.TransformDirection(_relativeSetpointPosition);
+            Vector3 currentPosition = transform.position;
+            Vector3 setpointPosition = cameraPosition + _mainCamTransform.TransformDirection(_relativeSetpointPosition);
             transform.position = Vector3.Lerp(currentPosition, setpointPosition, _lerpFactor * Time.deltaTime);
 
-            var lookRotation = Quaternion.LookRotation(cameraPosition - currentPosition);
-            var setpointRotation = Quaternion.Euler(0, lookRotation.eulerAngles.y, 0);
-            var currentRotation = transform.rotation;
+            Quaternion lookRotation = _faceAwayFromCamera 
+                ? Quaternion.LookRotation(currentPosition - cameraPosition)
+                : Quaternion.LookRotation(cameraPosition - currentPosition);
+            Quaternion setpointRotation = Quaternion.Euler(0, lookRotation.eulerAngles.y, 0);
+            Quaternion currentRotation = transform.rotation;
             transform.rotation = Quaternion.Slerp(currentRotation, setpointRotation, _lerpFactor * Time.deltaTime);
         }
     }

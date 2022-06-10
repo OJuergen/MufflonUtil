95082b95be1aa336b9ffea2e94998240c4726bbd
Ole <ole.juergensen87@gmail.com>
2022-06-10 14:38:42 +0200

    Add ScriptableObjectWithGUID

---
+++ Assets/MufflonUtil/Runtime/AssetManagement/ScriptableObjectWithGUID.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace MufflonUtil
+{
+    /// <summary>
+    /// Extension of <see cref="ScriptableObject"/> that stores the file GUID as a serialized field and exposes it as
+    /// a public property <see cref="GUID"/>.
+    /// This can be useful for identifying an asset over the network or from a generated file or database.
+    /// </summary>
+    public class ScriptableObjectWithGUID : ScriptableObject, ISerializationCallbackReceiver
+    {
+        /// <summary>
+        /// The Unity-generated file GUID. This is a unique identifier for the asset.
+        /// </summary>
+        [field: SerializeField, NotEditable] public string GUID { get; [UsedImplicitly] private set; }
+        
+        public void OnBeforeSerialize()
+        {
+#if UNITY_EDITOR
+            GUID = UnityEditor.AssetDatabase.AssetPathToGUID(UnityEditor.AssetDatabase.GetAssetPath(this));
+#endif
+        }
+
+        public void OnAfterDeserialize()
+        { }
+    }
+}

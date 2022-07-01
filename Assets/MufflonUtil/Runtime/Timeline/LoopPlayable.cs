9dda1ef1a0627e97ad6b8c83b3e7d9028cb3f522
Ole <ole.juergensen87@gmail.com>
2022-07-01 15:43:45 +0200

    stabilize loop playable

--- Assets/MufflonUtil/Runtime/Timeline/LoopPlayable.cs
+++ Assets/MufflonUtil/Runtime/Timeline/LoopPlayable.cs
@@ -21,15 +21,22 @@ namespace MufflonUtil
             private float _time;
             private bool _isInitialized;
             private bool _broken;
+            private DirectorWrapMode _directorWrapMode;
+            private PlayableAsset _playableAsset;
 
             public void Break()
             {
                 _broken = true;
             }
 
-            protected override void OnBehaviourStart(TimelineController playerData)
+            protected override void OnBehaviourStart(TimelineController timelineController)
             {
+                PlayableDirector playableDirector = timelineController.PlayableDirector;
+                _playableAsset = playableDirector.playableAsset;
                 _isInitialized = false;
+                _directorWrapMode = playableDirector.extrapolationMode;
+                // prevent director from stopping when timeline ends with loop
+                playableDirector.extrapolationMode = DirectorWrapMode.Hold;
             }
 
             protected override void OnBehaviourUpdate(Playable playable, FrameData info,
@@ -41,6 +48,9 @@ namespace MufflonUtil
                     _isInitialized = true;
                     timelineController.BreakingLoop += Break;
                 }
+
+                bool isOutOfClipNextFrame = timelineController.PlayableDirector.time - _time + info.deltaTime > playable.GetDuration();
+                if(!_broken && isOutOfClipNextFrame) timelineController.PlayableDirector.time = _time;
             }
 
             protected override void OnBehaviourStop(Playable playable, FrameData info,
@@ -51,8 +61,16 @@ namespace MufflonUtil
                 bool inClip = playableDirector.time - info.effectiveSpeed * info.deltaTime >= _time &&
                               playableDirector.time - info.effectiveSpeed * info.deltaTime <= _time + playable.GetDuration();
                 // Debug.Log($"Loop clip stopped: isBroken {_broken}, director state: {playableDirector.state}, inClip: {inClip}, deltaTime: {info.deltaTime}");
-                if (!_broken && playableDirector.state == PlayState.Playing && inClip) playableDirector.time = _time;
-                else timelineController.BreakingLoop -= Break;
+                if (!_broken && playableDirector.state == PlayState.Playing &&
+                    playableDirector.playableAsset == _playableAsset)
+                {
+                    playableDirector.time = _time;
+                }
+                else
+                {
+                    timelineController.BreakingLoop -= Break;
+                    timelineController.PlayableDirector.extrapolationMode = _directorWrapMode;
+                }
             }
         }
     }

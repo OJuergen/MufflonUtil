ed55a4ceadea0976c55d1c3dfc111fa1540e4bbf
Ole <ole.juergensen87@gmail.com>
2022-11-08 13:16:39 +0100

    rename ToHashSet to MakeHashSet to avoid conflicts with newer Linq

--- Assets/MufflonUtil/Runtime/EnumerableExtensions.cs
+++ Assets/MufflonUtil/Runtime/EnumerableExtensions.cs
@@ -5,12 +5,12 @@ namespace MufflonUtil
 {
     public static class EnumerableExtensions
     {
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
+        public static HashSet<T> MakeHashSet<T>(this IEnumerable<T> source)
         {
             return new HashSet<T>(source);
         }
 
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer)
+        public static HashSet<T> MakeHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer)
         {
             return new HashSet<T>(source, comparer);
         }

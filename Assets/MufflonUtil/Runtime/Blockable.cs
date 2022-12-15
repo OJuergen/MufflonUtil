2574c89b7ab6e85e1bef3977ef6b612df4945e61
Ole <ole.juergensen87@gmail.com>
2022-12-15 17:08:06 +0100

    standard component implementation of IBlockable

---
+++ Assets/MufflonUtil/Runtime/Blockable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MufflonUtil
+{
+    /// <summary>
+    /// A component implementation of the <see cref="IBlockable"/> interface, also offering serializable events.
+    /// Uses a <see cref="HashSet{T}"/> internally to keep track of blockers.
+    /// </summary>
+    public class Blockable : MonoBehaviour, IBlockable
+    {
+        private readonly HashSet<object> _blockers = new HashSet<object>();
+        public event Action Blocked;
+        public event Action Unblocked;
+        [SerializeField] private UnityEvent _blocked;
+        [SerializeField] private UnityEvent _unblocked;
+        public bool IsBlocked => _blockers.Any(blocker => blocker != null);
+
+        public void AddBlocker(object blocker)
+        {
+            if (_blockers.Contains(blocker)) return;
+            _blockers.RemoveWhere(b => b == null);
+            bool wasBlockedBefore = IsBlocked;
+            _blockers.Add(blocker);
+            if (!wasBlockedBefore)
+            {
+                Blocked?.Invoke();
+                _blocked?.Invoke();
+            }
+        }
+
+        public void RemoveBlocker(object blocker)
+        {
+            if (!_blockers.Contains(blocker)) return;
+            _blockers.RemoveWhere(b => b == null);
+            _blockers.Remove(blocker);
+            if (!IsBlocked)
+            {
+                Unblocked?.Invoke();
+                _unblocked?.Invoke();
+            }
+        }
+    }
+}
--- Assets/MufflonUtil/Runtime/IBlockable.cs
+++ Assets/MufflonUtil/Runtime/IBlockable.cs
@@ -1,26 +1,31 @@
+using System;
+
 namespace MufflonUtil
 {
     /// <summary>
-    /// An interactable object that can be blocked by others to prevent interaction.
+    /// An interactable object that can be blocked by others to prevent interaction until all blockers are removed.
     /// </summary>
     public interface IBlockable
     {
+        event Action Blocked;
+        event Action Unblocked;
+        
+        /// <summary>
+        /// Returns whether or not interaction with this object is currently blocked.
+        /// </summary>
+        /// <returns>True iff the object is blocked.</returns>
+        bool IsBlocked { get; }
+        
         /// <summary>
         /// Add an object as a blocker for interaction.
         /// </summary>
-        /// <param name="blocker">the object blocking interaction</param>
+        /// <param name="blocker">The object blocking interaction.</param>
         void AddBlocker(object blocker);
 
         /// <summary>
         /// Remove a blocker to free interaction.
         /// </summary>
-        /// <param name="blocker">the object blocking interaction</param>
+        /// <param name="blocker">The object blocking interaction.</param>
         void RemoveBlocker(object blocker);
-
-        /// <summary>
-        /// Returns whether or not interaction with this object is currently blocked.
-        /// </summary>
-        /// <returns></returns>
-        bool IsBlocked();
     }
 }

81bdb140acec329700c5201454f8e3a30162c271
Ole <ole@crumbling-game.com>
2024-06-28 12:38:37 +0200

    [feat] make FaceCamera work in edit mode

--- Assets/MufflonUtil/Runtime/FaceCamera.cs
+++ Assets/MufflonUtil/Runtime/FaceCamera.cs
@@ -2,6 +2,7 @@
 
 namespace MufflonUtil
 {
+    [ExecuteAlways]
     public class FaceCamera : MonoBehaviour
     {
         [SerializeField, Tooltip("If true, euler angle X will be set to zero")]
@@ -13,18 +14,20 @@ namespace MufflonUtil
         [SerializeField, Tooltip("Additional rotation applied to the object")]
         private Vector3 _offset;
 
-        private Camera _camera;
-
-        private void Start()
-        {
-            _camera = Camera.main;
-            enabled = _camera != null;
-        }
-
         private void LateUpdate()
         {
             if (!enabled) return; // in tests, LateUpdate gets called at least once even when script is disabled
-            Quaternion lookRotation = Quaternion.LookRotation(transform.position - _camera.transform.position);
+            Camera cam = Camera.main;
+            
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                cam = UnityEditor.SceneView.lastActiveSceneView?.camera;
+            }
+#endif
+
+            if (cam == null) return;
+            Quaternion lookRotation = Quaternion.LookRotation(transform.position - cam.transform.position);
             transform.rotation = Quaternion.Euler(
                 _fixXRotation ? 0 : lookRotation.eulerAngles.x,
                 _fixYRotation ? 0 : lookRotation.eulerAngles.y,
